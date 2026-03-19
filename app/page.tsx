163a7d48127f441c2da7b39c90f36f270bb00601
TheApro <webanonym2005@gmail.com>
2026-03-19 13:20:58 +0500

    siz

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-[#fdf7da] overflow-hidden">
       <main className="flex flex-col items-center justify-center p-24">
-        <Eye size="w-32 sm:w-40 md:w-[750px]" />{" "}
+        <Eye size="w-32 sm:w-40 md:w-[550px]" />{" "}
         <div className="text-zinc-800 text-sm font-light tracking-widest animate-pulse">
           IT IS WATCHING
         </div>
