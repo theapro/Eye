0aaa078022f2fe07353e9888e6f8a79927ab6d4c
TheApro <webanonym2005@gmail.com>
2026-03-19 13:26:50 +0500

    Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "IT IS WATCHING",
   description: "- APRO",
+  icons: {
+    icon: "/public/eyeiris2.png",
+  },
 };
 
 export default function RootLayout({
